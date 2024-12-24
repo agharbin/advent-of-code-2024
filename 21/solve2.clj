859b5348e5ae5a0ce580ce9408c5f635a6e54419
Adam Harbin <harbin.adam@gmail.com>
2024-12-24 17:52:09 -0600

    21 Part 2

--- 21/solve2.clj
+++ 21/solve2.clj
@@ -1,6 +1,5 @@
 (ns advent.2024.21.2
   (:require
-    [clojure.data.priority-map :as pm]
     [clojure.math.combinatorics :as combo]
     [clojure.string :as s]
     [util :as u]))
@@ -19,14 +18,14 @@
 (def input-file "input.dat")
 (def input-data (->> (slurp input-file) parse-input))
 
-;; Part 1
+;; Part 2
 
 (def keypad [[ \7  \8 \9]
              [ \4  \5 \6]
              [ \1  \2 \3]
              [nil  \0 \A]])
 
-(def d-pad [[nil \^ \A]
+(def dpad [[nil \^ \A]
             [ \< \v \>]])
 
 (def keypad-button->position
@@ -35,19 +34,11 @@
        (filter (fn [[k v]] (some? k)))
        (into {})))
 
-(def all-keypad-buttons (keys keypad-button->position))
-
-(def d-pad-button->position
-  (->> (for [r (range (count d-pad)) c (range (count (first d-pad)))]
-         [(get-in d-pad [r c]) [r c]])
+(def dpad-button->position
+  (->> (for [r (range (count dpad)) c (range (count (first dpad)))]
+         [(get-in dpad [r c]) [r c]])
        (filter (fn [[k v]] (some? k)))
        (into {})))
-
-(def all-d-pad-buttons (keys d-pad-button->position))
-
-(defn manhattan-distance [[r1 c1] [r2 c2]]
-  (+ (abs (- r1 r2))
-     (abs (- c1 c2))))
 
 (def button->offset
   {\> [0 1]
@@ -75,77 +66,52 @@
       (map vec)
       (map #(conj % \A)))))
 
-(defn shortest-d-pad-path [exploration-level button-1 button-2]
-  (if (zero? exploration-level)
-    (->> (possible-paths d-pad (d-pad-button->position button-1) (d-pad-button->position button-2))
-         (sort-by count)
-         first)
-    (->> (for [p (possible-paths d-pad (d-pad-button->position button-1) (d-pad-button->position button-2))]
-           [p
-            (apply concat
-             (for [[b1 b2] (->> (conj (seq p) \A) (partition 2 1))]
-               (shortest-d-pad-path (dec exploration-level) b1 b2)))])
-         (sort-by #(count (second %)))
-         first
-         first)))
+(declare compute-shortest-dpad-path-length)
 
-(def shortest-d-pad-paths
-  (->>
-    (for [b1 all-d-pad-buttons
-          b2 all-d-pad-buttons]
-      [[b1 b2] (shortest-d-pad-path 2 b1 b2)])
-    (into {})))
-
-(defn shortest-keypad-path [exploration-level button-1 button-2]
-  (if (zero? exploration-level)
-    (->> (possible-paths d-pad (d-pad-button->position button-1) (d-pad-button->position button-2))
-         (sort-by count)
-         first)
-    (->> (for [p (possible-paths keypad (keypad-button->position button-1) (keypad-button->position button-2))]
-           [p
-            (apply concat
-             (for [[b1 b2] (->> (conj (seq p) \A) (partition 2 1))]
-               (shortest-d-pad-path (dec exploration-level) b1 b2)))])
-         (sort-by #(count (second %)))
-         first
-         first)))
-
-(def shortest-keypad-paths
-  (->>
-    (for [b1 all-keypad-buttons
-          b2 all-keypad-buttons]
-      [[b1 b2] (shortest-keypad-path 3 b1 b2)])
-    (into {})))
-
-(declare compute-d-pad-path-length)
-
-(def compute-d-pad-distance
+(def find-shortest-dpad-path-between-buttons
   (memoize
     (fn [level button-1 button-2]
-      (compute-d-pad-path-length level (shortest-d-pad-paths [button-1 button-2])))))
+      (if (zero? level)
+        (->> (possible-paths dpad (dpad-button->position button-1) (dpad-button->position button-2))
+             (sort-by count)
+             first
+             count)
+        (->> (for [path (possible-paths dpad (dpad-button->position button-1) (dpad-button->position button-2))]
+               (compute-shortest-dpad-path-length (dec level) path))
+             (apply min))))))
 
-(def compute-d-pad-path-length
+(def compute-shortest-dpad-path-length
   (memoize
     (fn [level path]
+      (let [pairs (->> (conj (seq path) \A) (partition 2 1))]
+        (apply +
+          (for [[b1 b2] pairs]
+            (find-shortest-dpad-path-between-buttons level b1 b2)))))))
+
+(def find-shortest-keypad-path-between-buttons
+  (memoize
+    (fn [level button-1 button-2]
       (if (zero? level)
-        (count path)
-        (->> (conj (seq path) \A)
-             (partition 2 1)
-             (map (fn [[b1 b2]] (compute-d-pad-distance (dec level) b1 b2)))
-             (apply +))))))
+        (->> (possible-paths keypad (keypad-button->position button-1) (keypad-button->position button-2))
+             (sort-by count)
+             first
+             count)
+        (->>
+          (for [path (possible-paths keypad (keypad-button->position button-1) (keypad-button->position button-2))]
+            (compute-shortest-dpad-path-length (dec level) path))
+          (apply min))))))
 
-(defn compute-keypad-distance [num-d-pads button-1 button-2]
-  (compute-d-pad-path-length num-d-pads (shortest-keypad-paths [button-1 button-2])))
+(def compute-shortest-keypad-path-length
+  (memoize
+    (fn [level path]
+      (let [pairs (->> (conj (seq path) \A) (partition 2 1))]
+        (apply +
+          (for [[b1 b2] pairs]
+            (find-shortest-keypad-path-between-buttons level b1 b2)))))))
 
-(defn compute-keypad-path-length [num-d-pads path]
-  (->> (conj (seq path) \A)
-       (partition 2 1)
-       (map (fn [[b1 b2]] (compute-keypad-distance num-d-pads b1 b2)))
-       (apply +)))
-
-(defn solve-2 [input num-d-pads]
+(defn compute-score [level input]
   (apply +
     (for [[code value] input]
-      (* (compute-keypad-path-length num-d-pads code) value))))
+      (* value (compute-shortest-keypad-path-length level code)))))
 
-(solve-2 input-data 2)
+(compute-score 25 input-data)
